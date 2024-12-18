89ba93a874e9a3e3591b08be8e411dd4cca6f895
Matheus Fabião <matheusfabiao.dev@gmail.com>
2024-12-18 14:25:35 -0300

    feat: topic model

--- journeylogs/models.py
+++ journeylogs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    """Um assunto sobre qual o usuário está aprendendo."""
+
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Devolve uma representação em String do modelo."""
+        if len(str(self.text)) > 50:
+            return self.text[:50] + '...'
+        else:
+            return self.text
