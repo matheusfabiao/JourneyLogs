9bc691097f67427500b4f538989f3aaec37ce4b9
Matheus Fabião <matheusfabiao.dev@gmail.com>
2024-12-19 12:31:20 -0300

    feat: entry model and admin register

--- journeylogs/models.py
+++ journeylogs/models.py
@@ -13,3 +13,19 @@ class Topic(models.Model):
             return self.text[:50] + '...'
         else:
             return self.text
+
+class Entry(models.Model):
+    """Algo específico aprendido sobre um assunto."""
+    topic  = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'entries'
+        
+    def __str__(self):
+        """Devolve uma representação em String do modelo."""
+        if len(str(self.text)) > 50:
+            return self.text[:50] + "..."
+        else:
+            return self.text
